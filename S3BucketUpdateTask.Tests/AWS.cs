5c1cf7f21b0a8bb6b3c6dd788edd4a37e17d54f5
Lawrence Yi <lawrenceyi@gmail.com>
2015-01-08 18:44:57 -0800

    intial commit

---
+++ S3BucketUpdateTask.Tests/AWS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace S3BucketUpdateTask.Tests
+{
+    public class AWS
+    {
+        [Test]
+        public void ConnectionTest()
+        {
+            
+        }
+    }
+}
